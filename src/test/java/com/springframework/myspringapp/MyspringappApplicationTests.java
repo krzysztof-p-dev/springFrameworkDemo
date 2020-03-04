438a42063400453f6629149034b2df2c091482d2
Krzysiek <krzysztof.pyz@gmail.com>
2020-03-04 21:46:24 +0100

    First commit with structure

---
+++ src/test/java/com/springframework/myspringapp/MyspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
