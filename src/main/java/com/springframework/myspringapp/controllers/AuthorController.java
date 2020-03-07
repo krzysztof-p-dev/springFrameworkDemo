c291acf2ac5b355ab01a0988d9252b570daf5ab1
Krzysiek <krzysztof.pyz@gmail.com>
2020-03-07 13:21:57 +0100

    Author thymeleaf

---
+++ src/main/java/com/springframework/myspringapp/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package com.springframework.myspringapp.controllers;
+
+import com.springframework.myspringapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    public AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
