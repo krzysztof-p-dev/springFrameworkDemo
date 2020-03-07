fa5a2c50768c89dfe6081d2a19b21fe163e672bd
Krzysiek <krzysztof.pyz@gmail.com>
2020-03-07 11:59:14 +0100

    Spring MVC controller

---
+++ src/main/java/com/springframework/myspringapp/controllers/BookController.java
@@ -0,0 +1,20 @@
+package com.springframework.myspringapp.controllers;
+
+import com.springframework.myspringapp.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
